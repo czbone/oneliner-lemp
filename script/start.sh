4b61fe6c2cf3b7f42b84985664657bd69388f7de
naoki hirata <naoki@magic3.org>
2024-07-07 19:58:51 +0900

    タグなしの処理更新。

--- script/start.sh
+++ script/start.sh
@@ -81,6 +81,8 @@ else
     url=`curl -s "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/tags" | grep "tarball_url" | \
         sed -n '/[ \t]*"tarball_url"/p' | head -n 1 | \
         sed -e 's/[ \t]*".*":[ \t]*"\(.*\)".*/\1/'`
+echo $url
+echo '-----'
     version=`basename $url | sed -e 's/v\([0-9\.]*\)/\1/'`
 fi
 filename=${GITHUB_REPO}_${version}.tar.gz

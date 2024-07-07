69f9fa5b267e0386211a73f399ea3f69d06074f4
naoki hirata <naoki@magic3.org>
2024-07-07 20:03:03 +0900

    タグなしの処理更新。

--- script/start.sh
+++ script/start.sh
@@ -81,6 +81,8 @@ else
     url=`curl -s "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/tags" | grep "tarball_url" | \
         sed -n '/[ \t]*"tarball_url"/p' | head -n 1 | \
         sed -e 's/[ \t]*".*":[ \t]*"\(.*\)".*/\1/'`
+
+echo "@@${url}@@"
     if [ "${url}" == '' ]; then
         echo "Not found archive with tag."
         exit 1
